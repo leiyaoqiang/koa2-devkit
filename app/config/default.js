80e8d696fe634f3b730d838a8b7dcfd9ac253e4a
雷耀强 <1010486863@qq.com>
2016-10-18 17:08:43 +0800

    delete app

--- app/config/default.js
+++
@@ -1,3 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = {
-	port: 1991 };module.exports = exports["default"];
-//# sourceMappingURL=default.js.map
--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _koaRouter = require('koa-router');var _koaRouter2 = _interopRequireDefault(_koaRouter);
-var _indexCtrl = require('../controllers/indexCtrl');var _indexCtrl2 = _interopRequireDefault(_indexCtrl);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-var router = new _koaRouter2.default();
-
-router.get('/', _indexCtrl2.default);exports.default =
-
-router;module.exports = exports['default'];
-//# sourceMappingURL=index.js.map
